471ad49c1c661830363e87a0547314f00a1e03bb
anydream <anydreamplay@hotmail.com>
2017-09-27 16:20:31 +0800

    Add test case

--- tcmalloc_nohook/main.cpp
+++ tcmalloc_nohook/main.cpp
@@ -1,9 +1,40 @@
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
 
+#if 1
 #include <gperftools/tcmalloc.h>
+#define ALLOC	tc_malloc
+#define FREE	tc_free
+#else
+#include <stdlib.h>
+#define ALLOC	malloc
+#define FREE	free
+#endif
 
 int main()
 {
-	void *p = tc_malloc(256);
-	tc_free(p);
+	const char checkbuf[256] = { 1, 2, 3, 4 };
+
+	auto stt = clock();
+	size_t loops = 0;
+	for (;;)
+	{
+		void *p = ALLOC(sizeof(checkbuf));
+		memcpy(p, checkbuf, sizeof(checkbuf));
+		if (memcmp(p, checkbuf, sizeof(checkbuf)) != 0)
+		{
+			printf("Fail!\n");
+			break;
+		}
+		FREE(p);
+		++loops;
+
+		auto cut = clock();
+		if (cut - stt >= 5000)
+			break;
+	}
+	printf("Loops: %u\n", loops);
+
 	return 0;
 }
